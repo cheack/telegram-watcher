01f738e2bf44c4253f9e647dcf0937dd72cf3f96
Vladimir Pakhomchik <v.pahomchik@gmail.com>
2025-01-22 15:04:54 +0100

    Updated TelescopeServiceProvider

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -57,7 +57,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                env('TELESCOPE_ADMIN_EMAIL'),
+                config('telescope.admin_email'),
             ]);
         });
     }
