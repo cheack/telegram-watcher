5446299fb6bb62094924292e1e10759f1ebd1f46
Vladimir Pakhomchik <v.pahomchik@gmail.com>
2026-03-28 10:43:23 +0000

    Upgrade to Laravel 13, remove unused grkamil/laravel-telegram-logging

--- config/logging.php
+++ config/logging.php
@@ -127,11 +127,6 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
-        'telegram' => [
-            'driver' => 'custom',
-            'via' => Logger\TelegramLogger::class,
-            'level'  => 'debug',
-        ],
     ],
 
 ];
